4e284ad232acd844113ad276297f04775d71f7fc
KateKateKKK <kate@followup.usgit config --global user.name KateKateKKK>
2025-07-09 16:06:22 +0800

    initialize

--- config/db.go
+++ config/db.go
@@ -1,21 +1,41 @@
 package config
 
 import (
-	"gorm.io/driver/mysql"
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
+	"os"
 	"particlumn_backend/model"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "user:password@tcp(localhost:3306)/forum?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	log.Println("InitDB....")
+	if err := godotenv.Load(); err != nil {
+		panic("failed to load .env file: " + err.Error())
+	}
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	sslmode := os.Getenv("DB_SSLMODE")
+	timezone := os.Getenv("DB_TIMEZONE")
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		host, user, password, dbname, port, sslmode, timezone,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
 	if err != nil {
-		panic("failed to connect database: " + err.Error())
+		panic("failed to connect to PostgreSQL: " + err.Error())
+	}
+	if err := db.AutoMigrate(&model.User{}, &model.Forum{}, &model.UserFollowForum{}, &model.Post{}, &model.PostLike{}); err != nil {
+		panic("auto migration failed: " + err.Error())
 	}
 
-	// 自动迁移
-	db.AutoMigrate(&model.User{}, &model.Forum{}, &model.UserFollowForum{}, &model.Post{}, &model.PostLike{})
 	DB = db
 }
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"particlumn_backend/config"
 	"particlumn_backend/router"
 )
 
 func main() {
 	r := gin.Default()
+	config.InitDB()
 	router.InitRoutes(r)
 	r.Run(":8007")
 }
